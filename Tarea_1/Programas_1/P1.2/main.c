bd5e325f8464f34547d0255b5ab73865dc0a5c32
Yanet Encarnacion <ce22181104@gmail.com>
2025-09-18 00:28:11 -0400

    Archivos agregados

---
+++ Tarea_1/Programas_1/P1.2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* El segundo programa, invierte datos */
+
+int main(void)
+{
+    int A, B, C, D;
+    printf("Ingrese cuatro variables de tipo entero:\n");
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    printf ("\n %d %d %d %d ", D, C, B, A);
+    return 0;
+}
