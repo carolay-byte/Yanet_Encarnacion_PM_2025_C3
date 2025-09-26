faad3ef284243bf4f4c6b370fc318969aec6f025
Yanet Encarnacion <ce22181104@gmail.com>
2025-09-25 21:59:39 -0400

    Agregando Programas faltantes de Tarea 1, ahora esta completa

--- Tarea_1/Programas_1/P1.4/main.c
+++ Tarea_1/Programas_1/P1.4/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+ /* Superficie del triangulo.
+ El programa, al recibir como datos la base y la altura de un triangulo,
+ calcula su superficie.
+
+ BAS, ALT y SUP: variables de tipo real. */
+
+int main(void)
 {
-    printf("Hello world!\n");
+    float BAS, ALT, SUP;
+    printf("Ingrese la base y la altura del triangulo:!\n");
+    scanf("%f %f" , &BAS, &ALT);
+    SUP = BAS * ALT / 2;
+    printf("\nLa superficie del triangulo es: %5.2f" , SUP);
     return 0;
 }
