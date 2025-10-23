0c2b836dacfda5f104041bdaceb5c6d2962a9ae1
Yanet Encarnacion <ce22181104@gmail.com>
2025-10-23 14:05:37 -0400

    actualizando programas 7

--- Tarea_5/Programas_7/P7.8/main.c
+++ Tarea_5/Programas_7/P7.8/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int main()
+void minymay (char *cad);
+
+void main(void)
+
 {
-    printf("Hello world!\n");
-    return 0;
+    int i, n;
+    char FRA[20][50];
+
+    printf("\nIngrese el numero de filas del arreglo: ");
+    scanf("%d", &n);
+    for (i=0; i<n; i++)
+{
+    printf("\nIngrese la linea %d de texto: ", i+1);
+        fflush(stdin);
+        gets (FRA[i]);
+    }
+
+
+    printf("\n\n");
+     for (i=0; i<n; i++)
+        minymay (FRA[i]);
+}
+  void minymay(char *cadena)
+    {
+        int i = 0, mi = 0, ma = 0;
+        while(cadena[i] != '\0')
+        {
+            if (islower(cadena[i]))
+                mi++;
+            else
+                if (isupper(cadena[i]))
+                ma++;
+            i++;
+}
+        printf("\n\nNumero de letras minusculas; %d", mi);
+        printf("\nNumero de letras mayusculas; %d", ma);
 }
