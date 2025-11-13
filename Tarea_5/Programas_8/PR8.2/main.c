20b69e5c67c5a50f7527daa008088f0f8bf06ce4
Yanet Encarnacion <ce22181104@gmail.com>
2025-11-13 13:43:29 -0400

    Actualizando tarea

---
+++ Tarea_5/Programas_8/PR8.2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Escuela.
+El programa genera informacion importante de los alumnos de una escuela.
+
+typedef struct       /* Declaracion de la estructura matpro. */
+{
+char mat[20];    /* Materia. */
+int pro;          /* Promedio. */
+} matpro;
+
+t
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
